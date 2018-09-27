7adc0ee82a9901270120293923cd7bc0a9669a87
eddie0918 <eddie.arizon0918@gmail.com>
2018-09-27 05:53:52 +0200

    occasion select

--- src/Components/Purchase/DigitalCards.js
+++ src/Components/Purchase/DigitalCards.js
@@ -5,24 +5,13 @@ import { OccasionsList, CardDesignList, CardBackgroundSchemesList } from '../Moc
 import MainSection from '../MainTemplate/MainSection'
 import MainPage from '../MainTemplate/MainPage'
 import CardDetailForm from './DigitalComponents/CardDetailForm'
-import { withStyles } from '@material-ui/core/styles';
 import { Chip } from '@material-ui/core';
 import DoneIcon from '@material-ui/icons/Done';
-
-const styles = theme => ({
-  root: {
-    width: '90%',
-  },
-  resetContainer: {
-    padding: theme.spacing.unit * 3,
-  },
-});
 
 class DigitalCards extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      activeStep: 0,
       card: {
         occasion: OccasionsList[0],
         design: CardDesignList[0],
@@ -30,56 +19,37 @@ class DigitalCards extends React.Component {
       }
     };
   }
-  handleNext = () => {
-    this.setState(state => ({
-      activeStep: state.activeStep + 1,
-    }));
-  };
-
-  handleBack = () => {
-    this.setState(state => ({
-      activeStep: state.activeStep - 1,
-    }));
-  };
-
-  handleReset = () => {
-    this.setState({
-      activeStep: 0,
-    });
-  };
 
   updateCard = data => {
     this.setState({
       card: {...this.state.card, [data.key]: data.value}
     })
-    this.handleNext()
+    // this.handleNext() scroll to next step
   }
   render() {
-    const { classes } = this.props;
-    const { activeStep, card } = this.state;
-    console.log('active step', activeStep)
+    const { card } = this.state;
     return (
       <MainPage
         title='Digital Cards'
         description={<p>You can have cards shipped to you for in-person giving or shipped directly to the recipient. Want to order in bulk? Use our Custom Physical Cards page.</p>}
       >
-        <MainSection title="Choose the occasion" icon="list">
-          <div class="form-panel mx-5 container-fluid">
-            {OccasionsList.map((type, index) => {
+        <MainSection className="container-fluid" title="Choose the occasion" icon="list">
+          <div className="form-panel px-5 container-fluid">
+            {OccasionsList.map((occasion, index) => {
               return (
                 <Chip
                   key={index}
-                  label={type}
-                  onClick={() => this.updateCard({ key: 'occasion', value: type})}
+                  label={occasion.label}
+                  onClick={() => this.updateCard({ key: 'occasion', value: occasion})}
                   className="m-1"
-                  icon={type == this.state.card.occasion ? <DoneIcon /> : null}
+                  icon={occasion.value == this.state.card.occasion.value ? <DoneIcon /> : null}
                 />
               )
             })}
           </div>
         </MainSection>
-        <MainSection title="Choose a card image" icon="list">
-          <Row className="form-panel mx-5 container-fluid">
+        <MainSection className="container-fluid" title="Choose a card image" icon="list">
+          <div className="form-panel px-5 container-fluid d-flex flex-wrap">
             {CardDesignList.map((design, index) => {
               return (
                 <Col key={index} xs="6" sm="3" className="my-3">
@@ -89,19 +59,19 @@ class DigitalCards extends React.Component {
                 </Col>
               )
             })}
-          </Row>
+          </div>
         </MainSection>
-        <MainSection title="Choose a card background scheme" icon="list">
-          <div class="form-panel mx-5 container-fluid">
+        <MainSection className="container-fluid" title="Choose a card background scheme" icon="list">
+          <div className="form-panel px-5 container-fluid">
             {CardBackgroundSchemesList.map((scheme, index) => {
               return (
-                <ColorButton className="m-1" color={scheme.color} onClick={() => this.updateCard({ key: 'scheme', value: scheme})}>{scheme.name}</ColorButton>
+                <ColorButton key={index} className="m-1" color={scheme.color} onClick={() => this.updateCard({ key: 'scheme', value: scheme})}>{scheme.name}</ColorButton>
               )
             })}
           </div>
         </MainSection>
-        <MainSection title="Preview your card design" icon="list">
-          <div class="form-panel mx-5 container-fluid">
+        <MainSection className="container-fluid" title="Preview your card design" icon="list">
+          <div className="form-panel px-5 container-fluid">
             <Card style={{ backgroundColor: card.scheme.color, borderColor: card.scheme.color }}>
               <CardHeader className="text-center">Selected Design: <b>{card.design.id}</b></CardHeader>
               <CardBody className="p-5" style={{ backgroundColor: card.scheme.color, borderColor: card.scheme.color }}>
@@ -110,8 +80,8 @@ class DigitalCards extends React.Component {
             </Card>
           </div>
         </MainSection>
-        <MainSection title="Enter the card details" icon="list">
-          <div class="form-panel mx-5 mb-5 container-fluid">
+        <MainSection className="container-fluid" title="Enter the card details" icon="list">
+          <div className="form-panel px-5 mb-5 container-fluid">
             <CardDetailForm />
           </div>
         </MainSection>
@@ -124,4 +94,4 @@ const ColorButton = styled(Button)`
   background-color: ${props => props.color} !important
 `
 
-export default withStyles(styles)(DigitalCards);
+export default DigitalCards;
--- src/Components/Purchase/PrintCards.js
+++ src/Components/Purchase/PrintCards.js
@@ -1,14 +1,95 @@
 import React from 'react';
-import { Container, Row, Col } from 'reactstrap';
+import styled from 'styled-components'
+import { Button, Label, Col, Card, CardHeader, CardBody, CardImg } from 'reactstrap';
+import { OccasionsList, CardDesignList, CardBackgroundSchemesList } from '../MockData'
+import MainSection from '../MainTemplate/MainSection'
+import MainPage from '../MainTemplate/MainPage'
+import CardDetailForm from './DigitalComponents/CardDetailForm'
+import OccasionSelect from '../MiscComponents/OccasionSelect';
 
-export default function PrintCards(props) {
-  return <section>
-    <Container>
-      <Row>
-        <Col xs="12">
-          <div className="temp-filler"><p>This is the print cards page text.</p></div>
-        </Col>
-      </Row>
-    </Container>
-  </section>;
+class PrintCards extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      card: {
+        occasion: OccasionsList[0],
+        design: CardDesignList[0],
+        scheme: CardBackgroundSchemesList[0]
+      }
+    };
+  }
+  updateCard = data => {
+    this.setState({
+      card: {...this.state.card, [data.key]: data.value}
+    })
+    // this.handleNext() scroll to next step
+  }
+  render() {
+    const { card } = this.state;
+    return (
+      <MainPage
+        title='Printable Cards'
+      >
+        <MainSection className="container-fluid" title="Choose the occasion" icon="list">
+          <div className="form-panel px-5 container-fluid">
+
+            <div className="has-float-label d-block">
+              <OccasionSelect
+                selectedValue={card.occasion}
+                onChange={(selectedOccasion) => {
+                  console.log(selectedOccasion)
+                  card.occasion = selectedOccasion;
+                  this.setState({ card: card });
+                }} />
+              <Label for="occasionSelect" className="float-label-select">Select an occasion for this card...</Label>
+            </div>
+            
+          </div>
+        </MainSection>
+        <MainSection className="container-fluid" title="Choose a card image" icon="list">
+          <div className="form-panel px-5 container-fluid d-flex flex-wrap">
+            {CardDesignList.map((design, index) => {
+              return (
+                <Col key={index} xs="6" sm="3" className="my-3">
+                  <Card>
+                    <CardImg width="100%" src={design.ImageUrl} alt="Card image cap" onClick={() => this.updateCard({ key: 'design', value: design})} />
+                  </Card>
+                </Col>
+              )
+            })}
+          </div>
+        </MainSection>
+        <MainSection className="container-fluid" title="Choose a card background scheme" icon="list">
+          <div className="form-panel px-5 container-fluid">
+            {CardBackgroundSchemesList.map((scheme, index) => {
+              return (
+                <ColorButton className="m-1" color={scheme.color} onClick={() => this.updateCard({ key: 'scheme', value: scheme})}>{scheme.name}</ColorButton>
+              )
+            })}
+          </div>
+        </MainSection>
+        <MainSection className="container-fluid" title="Preview your card design" icon="list">
+          <div className="form-panel px-5 container-fluid">
+            <Card style={{ backgroundColor: card.scheme.color, borderColor: card.scheme.color }}>
+              <CardHeader className="text-center">Selected Design: <b>{card.design.id}</b></CardHeader>
+              <CardBody className="p-5" style={{ backgroundColor: card.scheme.color, borderColor: card.scheme.color }}>
+                <img width="100%" src={card.design.ImageUrl} alt="Card image cap" />
+              </CardBody>
+            </Card>
+          </div>
+        </MainSection>
+        <MainSection className="container-fluid" title="Enter the card details" icon="list">
+          <div className="form-panel px-5 mb-5 container-fluid">
+            <CardDetailForm />
+          </div>
+        </MainSection>
+      </MainPage>
+    )
+  }
 }
+
+const ColorButton = styled(Button)`
+  background-color: ${props => props.color} !important
+`
+
+export default PrintCards;
